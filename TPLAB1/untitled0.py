ccf178c386dcca21202cdcd4de8f80359f14e040
Martin Destefano <mdestefano@frba.utn.edu.ar>
2022-06-10 18:43:23 -0300

    TS3

---
+++ TPLAB1/untitled0.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  9 19:50:02 2022
+
+@author: Deste
+"""
+
+import numpy as np 
+import scipy.signal as sig 
+import math as m
+import splane as sp
+import matplotlib.pyplot as plt
+k = 1/4
+myFilter_hand = sig.TransferFunction([1,-0.2588/m.sqrt(2),0.2588**2], [1,0.2588/m.sqrt(2),0.2588**2])
+
+w, mag, phase = sig.bode(myFilter_hand)
+plt.figure()
+plt.title('Magnitude response')
+plt.xlabel('Angular frecuency')
+plt.ylabel('Magnitude[dB]')
+plt.semilogx(w, mag)  # Fase
+plt.show()
+
+plt.figure()
+plt.title('Phase response')
+plt.xlabel('Angular frecuency')
+plt.ylabel('Phase[deg]')
+plt.semilogx(w, phase)  # Fase
+plt.show()
